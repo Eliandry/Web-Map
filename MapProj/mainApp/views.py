90bbfd41389ea80a9326bb91e14fd662e417fdf1
Eliandry <andreymacn@gmail.com>
2023-10-03 20:12:56 +0300

    возможность регистрации юзеров

--- MapProj/mainApp/views.py
+++ MapProj/mainApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Country
-
+from django.contrib import auth
 
 def main(request):
     country = Country.objects.order_by("-population")
-    context={'country':country}
-    return render(request,"main.html",context)
+    context={'country':country,'username': auth.get_user(request).username}
+    return render(request,"index.html",context)
---
+++ MapProj/users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from mainApp.models import Account
+
+class UserCreateForm(UserCreationForm):
+    first_name=forms.CharField(max_length=25,required=True)
+    last_name=forms.CharField(max_length=30,required=True)
+    username=forms.EmailField(required=True)
+    class Meta:
+        model=User
+        fields=("username",'first_name','last_name',"password1","password2")
+    def save(self, commit=True):
+        user=super(UserCreateForm,self).save(commit=False)
+        user.first_name=self.cleaned_data["first_name"]
+        user.last_name=self.cleaned_data["last_name"]
+        Account.objects.create(name=user.first_name,ids=user.username)
+        print(user.username)
+        if commit:
+            user.save()
+        return user
